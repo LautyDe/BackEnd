06f0db06bea280859e281b42d8a47761e9fc4bad
Lautaro Demonte <lauty.d.p@gmail.com>
2022-09-12 18:15:41 -0300

    Corrijo la funcion save

--- desafio.js
+++ desafio.js
@@ -41,21 +41,43 @@ class Contenedor {
 
   async save(producto) {
     try {
+      /* verifico si existe archivo y sino creo uno nuevo */
       if (!this.exists(this.archivo)) {
         console.log(
-          `El archivo ${this.archivo} no existe, se crea archivo nuevo`
+          `No se encontro el archivo ${this.archivo}\n se procede a crear uno nuevo`
         );
         let arrayProductos = [];
+
         producto["id"] = 1;
         arrayProductos.push(producto);
-        console.log("Agregando su producto...");
+        console.log("se esta agregando el producto");
         await this.writeFile(this.archivo, arrayProductos);
-        console.log(`Se agrego su producto con el id: ${producto["id"]}`);
+        console.log(`Se agrego un nuevo producto con la id ${producto["id"]}`);
         return producto["id"];
       } else {
+        /*Si el archivo existe  primero se verifica si esta vacio*/
+        if (this.readFile(this.archivo)) {
+          const data = await this.readFile(this.archivo);
+          if (data.length === 0) {
+            /*si esta vacio  se le asigna la id 1 al primer producto*/
+            producto["id"] = 1;
+          } else {
+            /*Si tiene producto se le asigna la id siguente */
+            let ultimoId = data[data.length - 1].id;
+            producto["id"] = ultimoId + 1;
+          }
+          data.push(producto);
+          console.log("se esta agregando el producto");
+          /*se escribe el producto */
+          this.writeFile(this.archivo, data);
+          console.log(
+            `Se agrego un nuevo producto con la id ${producto["id"]}`
+          );
+          return producto["id"];
+        }
       }
     } catch (error) {
-      console.log(`Ocurrio un error:`, error.message);
+      console.log(error.message);
     }
   }
 
